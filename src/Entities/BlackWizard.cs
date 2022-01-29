c738e06ee82ccbc76085dd8c532a7f056c58232d
Suelen <97741736+csuelen@users.noreply.github.com>
2022-01-29 10:44:37 -0300

    Create BlackWizard.cs

---
+++ src/Entities/BlackWizard.cs
@@ -0,0 +1,26 @@
+namespace JogoHeroiDIO.src.Entities
+{
+    public class BlackWizard : Hero
+    {
+
+            public BlackWizard(string Name, int Level, string HeroType) {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        public override string Attack(){
+            return this.Name + " lançou magia";
+        }
+
+        public string Attack(int Bonus){
+
+            if (Bonus > 6){
+                return this.Name + " lançou magia superefetiva com bonus de  " + Bonus;
+            }
+            else{
+            return this.Name + " lançou magia força franca com bonus de" + Bonus;
+            }
+        }
+
+    }
+}
